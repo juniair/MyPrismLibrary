c0c6f4c722356a7a6c0629b6fcb8fd2cbee2c1fb
juniair <juniair.kim@gmail.com>
2017-04-12 20:23:14 +0900

    ImageProcessor 라이브러리를 이용하여 해결 할것
    이미지는 총 3장이 필요?

--- ImageMaskingApp/ImageMaskingApp.Module/ModuleModule.cs
+++ ImageMaskingApp/ImageMaskingApp.Module/ModuleModule.cs
@@ -1,21 +1,22 @@
-﻿using Prism.Modularity;
+﻿using ImageMaskingApp.Infra;
+using Prism.Modularity;
 using Prism.Regions;
 using System;
 
 namespace ImageMaskingApp.Module
 {
-    public class Module : IModule
+    public class ModuleModule : IModule
     {
         IRegionManager _regionManager;
 
-        public Module(IRegionManager regionManager)
+        public ModuleModule(IRegionManager regionManager)
         {
             _regionManager = regionManager;
         }
 
         public void Initialize()
         {
-            throw new NotImplementedException();
+            _regionManager.RegisterViewWithRegion(RegionNames.ContentRegion, typeof(Views.Content));
         }
     }
 }
---
+++ ImageMaskingApp/ImageMaskingApp.Module/ViewModels/ContentViewModel.cs
@@ -0,0 +1,22 @@
+﻿using ImageProcessor;
+using ImageProcessor.Imaging;
+using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+
+namespace ImageMaskingApp.Module.ViewModels
+{
+    public class ContentViewModel : BindableBase
+    {
+        public ContentViewModel()
+        {
+            var mask = new ImageLayer();
+            mask.Position = new Point()
+            var factory = new ImageFactory();
+            new ImageProcessor.ImageFactory().Mask()
+        }
+    }
+}
